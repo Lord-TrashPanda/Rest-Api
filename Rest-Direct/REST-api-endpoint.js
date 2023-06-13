c4b35ecd2ad23dc0a61a456e6e3026c6eb26d915
Lord-TrashPanda <115721303+Lord-TrashPanda@users.noreply.github.com>
2023-06-13 13:34:32 +0100

    Update REST-api-endpoint.js

--- Rest-Direct/REST-api-endpoint.js
+++ Rest-Direct/REST-api-endpoint.js
@@ -2,7 +2,7 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-const uri = 'mongodb+srv://zane_adamson:Millb0y2@cluster1.rjwimzh.mongodb.net/';
+const uri = 'mongodb+srv://zane_adamson:Millb0y2@cluster1.rjwimzh.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
 let db;
 
